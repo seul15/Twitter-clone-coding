8132bc46480a67c0298e465f9a763aed0bd83d83
seul <angel3030303@gmail.com>
2022-01-27 20:30:35 +0900

    message page- chatting section

--- src/pages/Messages.vue
+++ src/pages/Messages.vue
@@ -3,7 +3,7 @@
     <!-- chat list -->
     <div class="w-2/5 border-r border-gray-100 overflow-y-auto">
       <div class="flex flex-col justify-between ">
-        <div class="p-3 font-bold text-lg border-b border-gray-100">쪽지</div>
+        <div class="p-3 font-bold text-lg border-b h-14 border-gray-100">쪽지</div>
         <!-- a chat list -->
         <div class="flex px-3 py-4 hover:bg-gray-200 border-b border-gray-100 items-center" v-for="message in 10" :key="message">
           <img src="http://picsum.photos/200" class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80 mr-3">
@@ -19,7 +19,55 @@
       </div>
     </div>
     <!-- chatting -->
-    <div class="w-3/5 border-r border-gray-100"></div>
+    <div class="w-3/5 border-r border-gray-100">
+      <div class="flex flex-col h-screen">
+        <!-- title -->
+        <div class="flex px-3 h-14 border-b border-gray-100 items-center">
+          <img src="http://picsum.photos/200" class="w-8 h-8 rounded-full mr-2 hover:opacity-80 cursor-pointer">
+          <div>
+            <div class="font-bold text-lg">닉네임</div>
+            <div class="text-sm text-gray-500">@아이디</div>
+          </div>
+        </div>
+        <!-- user info -->
+        <div class="flex flex-col justify-center items-center border-b border-gray-100 hover:bg-gray-50 py-6">
+          <div>
+            <span class="font-bold">닉네임</span>
+            <span class="text-gray-500">@아이디</span>
+          </div>
+          <div>
+            <span class="font-bold mr-1">28</span><span class="text-gray-500">팔로우 중</span>
+            <span class="font-bold ml-3 mr-1">7</span><span class="text-gray-500">팔로워</span>
+          </div>          
+          <div>
+            <span class="text-gray-500 mr-1">가입일:</span>
+            <span class="text-gray-500">2011년 10월</span>
+          </div>
+        </div>
+        <div class="flex-1 overflow-y-scroll">
+          <!-- chat bubble : my chat -->
+          <div class="text-right px-3 py-3">
+            <span class="bg-primary text-white px-4 py-2 rounded-full">메세지</span>
+            <div class="text-gray-500 text-xs mt-2">2022년 1월 27일 오후 7:35</div>
+          </div>
+          <!-- chat bubble : opponent chat -->
+          <div class="text-left px-3 py-3" v-for="chat in 20" :key="chat">
+            <span class="bg-gray-100 px-4 py-2 rounded-full">메세지</span>
+            <div class="text-gray-500 text-xs mt-2">2022년 1월 27일 오후 7:35</div>
+          </div>
+        </div>
+
+        <!-- chat input -->
+        <div class="flex items-center bg-white border-t border-gray-100 sticky">
+        <!-- sticky - 고정하는 역할  -->
+          <input type="text" class="m-2 py-1 px-4 rounded-full bg-gray-100 resize-none outline-none flex-1" placeholder="새 쪽지 작성하기"/>
+          <button class="text-center">
+            <i class="far fa-paper-plane text-primary text-lg hover:bg-blue-50 p-2 rounded-full w-10 h-10"></i>
+          </button>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
