423a370bc323b9c8dd47f306eca38568441d0738
seul <angel3030303@gmail.com>
2022-02-02 20:24:39 +0900

    register page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Register page</h1>
+  <div class="flex flex-col items-center space-y-4 mt-10">
+    <i :class="`fab fa-twitter text-4xl text-primary ${loading ? 'animate-bounce' : ''}`"></i>
+    <span class="text-2xl font-bold">뜨위떠 회원가입</span>
+    <input v-model="email" type="text" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="이메일"/>
+    <input v-model="id" type="text" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="아이디"/>
+    <input v-model="password" type="text" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="비밀번호"/>
+    <button class="w-96 rounded-full bg-primary text-white py-4 hover:bg-dark" @click="onRegister">회원가입</button>
+    <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
+  setup() {
+    const email = ref('')
+    const id = ref('')
+    const password = ref('')
+    const loading = ref(false)
 
+    const onRegister = () => {
+      console.log(email.value, id.value, password.value)
+    }
+    return {email,id,password,loading,onRegister}
+  }
 }
 </script>
 
