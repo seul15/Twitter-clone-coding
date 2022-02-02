867a07b21bb6d406df7b01e36a9b987209fd0dd4
seul <angel3030303@gmail.com>
2022-02-03 03:17:34 +0900

    firebase auauthentication

---
+++ src/firebase/index.js
@@ -0,0 +1,22 @@
+// v9 compat packages are API compatible with v8 code
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmGRwz2pIGOMMS76MqxB2ypuIfsP5E3qc",
+  authDomain: "twitter-clone-4fb83.firebaseapp.com",
+  projectId: "twitter-clone-4fb83",
+  storageBucket: "twitter-clone-4fb83.appspot.com",
+  messagingSenderId: "262684529462",
+  appId: "1:262684529462:web:7411087423a3847fb6d997",
+  measurementId: "G-K6EVKDH2KJ"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+export const storage = firebase.storage()
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="flex flex-col items-center space-y-4 mt-10">
-    <i :class="`fab fa-twitter text-4xl text-primary ${loading ? 'animate-bounce' : ''}`"></i>
+    <i
+      :class="`fab fa-twitter text-4xl text-primary ${
+        loading ? 'animate-bounce' : ''
+      }`"
+    ></i>
     <span class="text-2xl font-bold">뜨위떠 회원가입</span>
-    <input v-model="email" type="text" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="이메일"/>
-    <input v-model="id" type="text" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="아이디"/>
-    <input v-model="password" type="text" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="비밀번호"/>
-    <button class="w-96 rounded-full bg-primary text-white py-4 hover:bg-dark" @click="onRegister">회원가입</button>
+    <input
+      v-model="username"
+      type="text"
+      class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none"
+      placeholder="아이디"
+    />
+    <input
+      v-model="email"
+      type="text"
+      class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none"
+      placeholder="이메일"
+    />
+    <input
+      v-model="password"
+      type="password"
+      class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none"
+      placeholder="비밀번호"
+    />
+    <button
+      class="w-96 rounded-full bg-primary text-white py-4 hover:bg-dark"
+      @click="onRegister"
+    >
+      회원가입
+    </button>
     <router-link to="/login">
       <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
     </router-link>
@@ -13,22 +37,30 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { auth } from "../firebase";
 export default {
   setup() {
-    const email = ref('')
-    const id = ref('')
-    const password = ref('')
-    const loading = ref(false)
+    const username = ref("");
+    const email = ref("");
+    const password = ref("");
+    const loading = ref(false);
 
-    const onRegister = () => {
-      console.log(email.value, id.value, password.value)
-    }
-    return {email,id,password,loading,onRegister}
-  }
-}
+    const onRegister = async () => {
+      try {
+        const credential = await auth.createUserWithEmailAndPassword(
+          email.value,
+          password.value
+        );
+        console.log(credential);
+      } catch (e) {
+        //console.log('create user with email and password error:',e)
+        alert(e.message);
+      }
+    };
+    return { email, username, password, loading, onRegister };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
