408468ea4dbe1396627c98b29efb5311b5f42948
seul <angel3030303@gmail.com>
2022-02-08 18:53:06 +0900

    comment modal popup

---
+++ src/components/CommentModal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="fixed z-10 inset-0 overflow-y-auto" @click="$emit('close-modal')">
+    <!-- $emit('') - '' 이벤트를 부모에게 방출 -->
+    <div class="flex justify-center min-h-screen sm:pt-6 sm:pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+      </div>
+
+      <!-- contents -->
+      <div @click.stop class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+        <!-- @click.stop 클릭이 적용되지 않음 -->
+        <div class="border-b border-gray-100 p-2 flex items-center justify-between">
+          <button @click="$emit('close-modal')" class="fas fa-times text-primary test-lg p-2 h-10 w-10 hover:bg-blue-50 rounded-full"></button>
+          <!-- tweet button -->
+          <div class="text-right sm:hidden mr-2">
+            <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+            <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+            <!-- padding -> 내부 간격 설정 / margin -> 외부 간격 설정 -->
+          </div>
+        </div>
+        <div>
+          <!-- origin tweet -->
+          <div class="flex px-4 pt-4 pb-3">
+            <div class="flex flex-col">
+              <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
+              <div class="ml-5 w-0.5 h-full bg-gray-300 mt-2 -mb-1"></div>
+            </div>
+            <div class="ml-2 flex-1">
+              <div class="flex space-x-2">
+                <span class="font-bold text-sm">닉네임</span>
+                <span class="text-gray text-sm">@이메일</span>
+                <span class="text-gray text-sm">1시간</span>
+              </div>
+              <div class="text-sm">내용~</div>
+              <div>
+                <span class="text-primary text-sm">@이메일</span>
+                <span class="text-gray text-sm"> 님에게 보내는 답글</span>
+              </div>
+            </div>
+          </div>
+          <!-- tweeting section -->
+          <div class="flex px-4 pb-4">
+            <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
+            <div class="ml-2 flex-1 flex-col">
+              <textarea v-model="tweetBody" rows="5" placeholder="내 답글을 트윗합니다" class="w-full text-lg font-bold focus:outline-none resize-none"></textarea>
+              <!-- 안드로이드 스튜디오에서 texthint 같은 느낌 -->
+              <!-- tweet button -->
+              <div class="text-right hidden sm:block">
+                <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">답글</button>
+                <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">답글</button>
+                <!-- padding -> 내부 간격 설정 / margin -> 외부 간격 설정 -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import {ref, computed} from 'vue'
+import addTweet from "../utils/addTweet";
+import store from "../store";
+
+export default {
+  setup(props,{emit}){
+    const tweetBody = ref('')
+    const currentUser = computed(()=>store.state.user)
+    const onAddTweet = async () => {
+      try {
+        await addTweet(tweetBody.value,currentUser.value);
+        tweetBody.value='';
+        emit('close-modal')
+      } catch (e) {
+        console.log("on add tweet error on homepage:", e);
+      }
+    };
+
+    return{
+      tweetBody, onAddTweet, currentUser
+    }
+  }
+}
+</script>
+<style></style>
